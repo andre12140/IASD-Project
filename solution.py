b2a44fb4277e46e9025054615e200eac849d9647
danieltarita <9cdanielsilva@gmail.com>
2020-11-03 19:22:23 +0000

    Results() is done. Fixed bugs with Actions(). Cost() is done. Gets a solution, but not the best one!

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import sys
 
 from search import Problem
-from utils import is_in
+
 import itertools
 
 class PMDAProblem(Problem):
@@ -17,13 +17,13 @@ class PMDAProblem(Problem):
     def set_initial_state(self):
         init_state = (0,) # Start at timestamp zero
         for m in self.M:
-            init_state = init_state + (("empty" , self.M[m],),)
+            init_state = init_state + ("empty" ,)
 
         for p in self.P:
             p_label = self.P[p][1];
             p_max_wait_time = self.L[p_label][0]
             p_current_wait_time = self.P[p][0]
-            p_availabe_wait_time = int(p_max_wait_time) - int(p_current_wait_time)
+            p_availabe_wait_time = p_max_wait_time - p_current_wait_time
 
             p_consult_time_left = self.L[p_label][1]
             init_state = init_state + ((p_availabe_wait_time, p_consult_time_left,),)
@@ -55,8 +55,13 @@ class PMDAProblem(Problem):
     def actions(self, state):
         #NOTE! TRY TO RETURN ITERATOR INSTEAD OF LIST #
         """ Returns all the possible actions from a given state. Has in account patient
-            consult time left ( If a patient has been attended for atleat his consult time,
+            consult time left ( If a patient has been attended for at least his consult time,
             won't be assigned to new medics) """
+
+        # Validate state before calculating new actions. Invalid state -> empty list of actions, won't expand the node
+        """for i in range(1 , self.P.__len__() +1):
+            if self.get_patient_from_state(state, i)[0] <0: # Atleast one patient has reached 0 minutes left to wait -> Invalid state
+                return []"""
 
         list_p = [] # List of patients tha need medical atention
         list_m = list(self.M.keys()) # List of medics that can give consults (In this case all medics)
@@ -64,7 +69,7 @@ class PMDAProblem(Problem):
         # Checks in the current state wich patients have consult time > 0
         for idx in range(1, self.P.__len__() +1):
             p = self.get_patient_from_state(state, idx)
-            if int(p[1]) > 0: # Patient consult time (left)
+            if p[1] > 0: # Patient consult time (left)
                 list_p.append(list(self.P.keys())[idx-1])
 
         # If there are more medics than patients waiting, adds empty slot to be assigned to medics with no patients
@@ -76,9 +81,27 @@ class PMDAProblem(Problem):
         combos_m = [tuple(list_m)]
         combos_p = itertools.permutations(list_p, num_medics)
         combos_p =  [t for t in (set(tuple(i) for i in combos_p))] # Remove duplicates
-        prod = itertools.product(combos_m, combos_p)
+        actions = itertools.product(combos_m, combos_p)
 
-        return [tuple(zip(i[0], i[1])) for i in prod]
+        actions_list = [tuple(zip(i[0], i[1])) for i in actions] # List of all possible actions without filtering actions that lead to unfeasible states
+        actiom_list_copy = actions_list.copy()
+
+        # Removes all states that result in infeasible state
+        for action in actiom_list_copy:
+            attended_patient = [i[1] for i in action if i[1] != "empty"] # Gets codes of patients being attended
+            attended_patient_idx = [list(self.P.keys()).index(code) for code in attended_patient] # Converts code to idx
+
+            all_index = list(range(0, self.P.__len__())) # Generates a list with all indexes
+            [all_index.remove(idx) for idx in attended_patient_idx] # Removes attended patient indexes from the list
+
+            for i in all_index:
+                if self.get_patient_from_state(state,i+1)[0] == 0 and self.get_patient_from_state(state,i+1)[1] > 0 : # If current patient has zero minutes and not being attended
+                    actions_list.remove(action)
+                    break
+
+
+        # Filtrar acções que resultam num estado inválido!
+        return actions_list
 
 
     def goal_test(self, state):
@@ -86,27 +109,33 @@ class PMDAProblem(Problem):
         state to self.goal or checks for state in self.goal if it is a
         list, as specified in the constructor. Override this method if
         checking against a single self.goal is not enough."""
-        if isinstance(self.goal, list):
-            return is_in(state, self.goal)
-        else:
-            return state == self.goal
+
+        for i in range(1, self.P.__len__()+1):
+             if self.get_patient_from_state(state, i)[1] > 0: # Checks if atleast one patient has consult time > 0
+                 return False
+
+        return True # Goal state -> all patients were attended
 
     def path_cost(self, c, state1, action, state2):
         """Return the cost of a solution path that arrives at state2 from
-        state1 via action, assuming cost c to get up to state1. If the problem
-        is such that the path doesn't matter, this function will only look at
-        state2. If the path does matter, it will consider c and maybe state1
-        and action. The default method costs 1 for every step in the path."""
-        return c + 1
+        state1 via action, assuming cost c to get up to state1."""
+        sum = 0;
+        for i in range(1, self.P.__len__()+1): # Sums the squares of all patients total waiting time
+            label_code = list(self.P.values())[i-1][1]
+            max_wait_time = self.L[label_code][0]
+            sum += pow(max_wait_time - self.get_patient_from_state(state2, i)[0], 2) # Square of patient total waiting time
+
+        return sum # Contemplates the total cost from the initial state until state 2
 
     def result(self, state, action):
         """A medic can either attend or not attend a patient
                 If he attends, Patient Consult time decreases by t*me.
                 If he doesn't attend, Patient Waiting time increases by t."""
 
-        new_state = (state[0] + self.t,) # timestamp constant
+        new_state = [state[0] + self.t] # timestamp constant
 
-        # [TO DO !] ADD MEDIC NEW STATES TO CURRENT STATE -------------
+        # Fill the(List) state with default value for the medics
+        new_state.extend(self.M.__len__() * " ")
 
         for i in range(1, self.P.__len__()+1):
             patient_state = self.get_patient_from_state(state, i)
@@ -114,16 +143,25 @@ class PMDAProblem(Problem):
             #if any(list(self.P.keys())[i] in j for j in action):
             try:
                 medic_code = next(name for (name, number) in action if number == list(self.P.keys())[i-1])
-                new_patient_conslt_time = float(self.get_patient_from_state(state, i)[1]) - (float(self.M[medic_code]) * self.t) # Consult time of patient decreases by (me * t)
+                new_state[(list(self.M.keys()).index(medic_code) + 1)] = list(self.P.keys())[i-1] # Updates list of state w/ new medic values (patient code of medic(i) )
+
+                new_patient_conslt_time = patient_state[1] - (self.M[medic_code] * self.t) # Consult time of patient decreases by (me * t)
+                new_state.append((patient_state[0],  new_patient_conslt_time)) # Updates list of state w/ new patient values;  Patiente waiting time = previous state
+
             except: # Patient not assigned to a medic by the given action
-                new_patient_remaining_time = float(self.get_patient_from_state(state, i)[0]) - self.t # Time left for the patient to be attended decreases by 't'
+                if patient_state[1] >0: # If patient consult time is greater than zero, change waiting time
+                    new_patient_remaining_time = patient_state[0] - self.t # Time left for the patient to be attended decreases by 't'
+                    new_state.append((new_patient_remaining_time, patient_state[1])) # Updates list of state w/ new patient values; Consult time = previous state
+                else:
+                    new_state.append(patient_state) # If consult time <0, doesn't change state
+
                 continue
 
-            # [TO DO !] ADD SAVE IN STATE [IN HERE!] -------------
+        for i in range(1, self.M.__len__()+1):
+           if new_state[i] == ' ' : new_state[i] = "empty"
 
 
-        """RETURNS state"""
-        pass
+        return tuple(new_state)
 
     # Loads a problem from a (opened) file object f (see below for format specification)
     def load(self, file_obj):
@@ -133,18 +171,22 @@ class PMDAProblem(Problem):
             if line == "\n":  # ignores blank line
                 continue
             elif line_strs[0] == "MD":
-                self.M[line_strs[1]] = line_strs[2]
+                self.M[line_strs[1]] = float(line_strs[2])
             elif line_strs[0] == "PL":
-                self.L[line_strs[1]] = (line_strs[2], line_strs[3])
+                self.L[line_strs[1]] = (float(line_strs[2]), float(line_strs[3]))
             elif line_strs[0] == "P":
-                self.P[line_strs[1]] = (line_strs[2], line_strs[3])
+                self.P[line_strs[1]] = (float(line_strs[2]), line_strs[3])
 
-
+from search import depth_first_graph_search
+from search import depth_first_tree_search, uniform_cost_search
 if __name__ == '__main__':
     pmda = PMDAProblem(sys.argv)
 
-    action = pmda.actions(pmda.initial)
-    pmda.result(pmda.initial, action[0])
-    breaksa = 1;
+    result = depth_first_graph_search(pmda)
+    #result = depth_first_tree_search(pmda)
+
+
+    #result = uniform_cost_search(pmda, display=True)
+    end =1;
 
 
