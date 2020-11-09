329e7b298ddb0db679cf3a3d53f7d96fa73374f9
danieltarita <9cdanielsilva@gmail.com>
2020-11-09 17:38:05 +0000

    Tryed to filter actions that result in same state. Half implemented...

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
-import NewSolution
+import solution
 import sys
 import time
 
 if __name__ == '__main__':
-    pmda = NewSolution.PDMAProblem() # Initiates the PMDAProblem object
+    pmda = solution.PDMAProblem() # Initiates the PMDAProblem object
     pmda.load(open("demo.txt", "r"))
     start = time.time()
     pmda.search() # Searches for a solution
-    print("\n---INFO---\nTime of execution:", round(time.time()-start, 2) ,"s" )
+    print("\n---INFO---\nTime of execution:", round(time.time()-start, 3) ,"s" )
     f = open("solution.txt", "w")
     pmda.save(f)    # Saves the actions that lead to the solution obtained
     try: print("Path Cost:", pmda.solution_node.path_cost)
